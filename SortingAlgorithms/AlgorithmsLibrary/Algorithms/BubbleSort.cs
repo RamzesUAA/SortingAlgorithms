badd67e0d212c7fefa108aff032c6290dc069c2a
Roman <ramzesball@gmail.com>
2020-09-28 13:24:39 +0300

    Realized bubble sort

---
+++ SortingAlgorithms/AlgorithmsLibrary/Algorithms/BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsLibrary.Algorithms
+{
+    public class BubbleSort
+    {
+        private void Swap(ref int lhs, ref int rhs)
+        {
+            int temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+        public void bubbleSort(ref int[] arr)
+        {
+            for(int i = 0; i<arr.Length; ++i)
+            {
+                for(int j = 0; j< arr.Length -1; ++j)
+                {
+                    if(arr[j] > arr[j+1])
+                    {
+                        Swap(ref arr[j], ref arr[j+1]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -0,0 +1,35 @@
+﻿using AlgorithmsLibrary.Algorithms;
+using System;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        static void InputFunc(int [] arr)
+        {
+            for(int i = 0; i < arr.Length; ++i)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter dimension of array:");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            int[] Array = new int[number];
+            Console.WriteLine("Enter numbers for array:");
+            for(int i = 0; i < number; ++i)
+            {
+                Console.Write($"Array[{i}]:");
+                input = Console.ReadLine();
+                Array[i] = int.Parse(input);
+            }
+
+            BubbleSort bubble = new BubbleSort();
+            bubble.bubbleSort(ref Array);
+            InputFunc(Array);
+        }
+    }
+}
